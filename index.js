fed40c3369edc6804069b63fc10262984d761388
preciouisaac <isaacpreciouschampion@gmail.com>
2023-09-26 12:03:35 +0100

    jswork

---
+++ index.js
@@ -0,0 +1,111 @@
+// console.log = ("Hello world")
+
+// //string method 
+// let firstName = "Favour"
+
+// console.log(firstName.toLowerCase());
+
+//toUppercase
+// let class_Name = "prosper"
+// console.log(class_Name.toUpperCase());
+
+// //Length
+// console.log(class_Name.length)
+
+// //indexof
+// console.log(class_Name.lastIndexOf("p"));
+
+// let newName = "nonso"
+
+// console.log(newName.charAt(4))
+
+// let secondName = "Daniella"
+// console.log(secondName.lastIndexOf("l"));
+
+// charCodeAt
+// console.log(secondName.charCodeAt("i"));
+
+// let recordName = "Freedom Park"
+// console.log(recordName.slice(0,7));
+// console.log(recordName.slice(8,12));
+
+// Classwork
+// Cut a value from the middle to the end
+// let newName = "Breakfast"
+// console.log(newName.slice(5,9));
+
+// let newName = "Breakfast"
+// console.log(newName.charAt(2));
+// let newName = "Breakfast"
+// console.log(newName.matchAll(2,4));
+
+
+// const userInput=("Breakfast");
+
+// const inputLength= userInput.length;
+
+// const minpoint = Math.floor(inputLength/4);
+
+// const half= userInput.slice(minpoint);
+
+// console.log(half)
+
+
+
+// let newNumber = Math.floor(Math.random())
+// console.log(newNumber);
+
+
+// Array Methods
+
+// let name = ["Man", 23, true, [2, false]];
+// let arr = Array("Nile", 3, true, ["gold", null]);
+
+// console.log(name)
+// console.log(name)
+
+
+// console.log(name[0])
+
+// let name = ["Man", 23, true, [2, false], ["food",20]];
+// let arr = Array("Nile", 3, true, ["gold", null]);
+
+// console.log(3[2])
+
+// reverse
+
+let name = ["finbarr"];
+let reversedArr = name.reverse();
+console.log(reversedArr);
+  
+
+let fruits = [];
+
+fruits.push["banana"]
+fruits.push["orange"]
+fruits.push["mango"]
+fruits.push;["banana"]
+console.log(fruits)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
